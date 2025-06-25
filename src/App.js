13de473999e506554a24e01dca918b1a8b17a746
MohammedISec <m7mdm.khalid@gmail.com>
2025-06-25 12:56:27 +0400

    save

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             {/* <Route path="/" element={<Layout />}> */}
-            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/blogs" element={<Blogs />} />
             <Route path="/contact" element={<Contact />} />
             {/* <Route path="*" element={<NoPage />} /> */}
